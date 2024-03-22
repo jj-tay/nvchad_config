e80d1c4df74bcbcc1e4f0d33738ea6e88387326c
Tay Jun Jie <jjat1987@gmail.com>
2024-03-22 11:31:40 +0800

    Debug vim-tmux-navigator

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,11 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+if os.getenv("TMUX") then
+  map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
+  map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
+  map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
+  map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
+end
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
